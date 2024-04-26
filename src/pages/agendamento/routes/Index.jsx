d954f0a66d7f484328026835af2fe513f6e45775
Rafael G. Francisco <Rafaelgfra@hotmail.com>
2024-04-26 12:11:39 -0300

    Correção de erro de rota

--- src/pages/agendamento/routes/Index.jsx
+++ src/pages/agendamento/routes/Index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import FormAgendamento from '../subpages/FormularioAgendamento.jsx'
-import MainAgendamento from '../Index.jsx'
 
 const Index = () => {
   return (
--- src/pages/internacao/routes/Index.jsx
+++ src/pages/internacao/routes/Index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import FormInternacao from '../subpages/Formulario.jsx'
-import MainInternacao from '../Index.jsx'
 
 const Index = () => {
   return (
